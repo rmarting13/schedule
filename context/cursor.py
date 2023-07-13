c67dc61d1785259c58f9aaaed60caa1577041516
Martin Gonzalez <rmarting13@gmail.com>
2023-07-13 02:30:00 -0300

    Creación de la clase Cursor.

---
+++ context/cursor.py
@@ -0,0 +1,51 @@
+from mysql_connection import Connection
+
+
+class Cursor:
+    """
+    Clase encargada de administrar el objeto cursor() del pool de conexiones, y las conexiones
+    individuales del pool (liberar las conexiones en deshuso y regresarlas al pool de conexiones)
+    """
+    def __init__(self):
+        self._conn = None
+        self._cursor = None
+
+# Los siguientes métodos __enter__ y __exit__ se encargan de abrir y cerrar los recursos cuando
+# se ejecuta una sintaxis WITH para usar una instancia de esta clase
+    def __enter__(self):
+        """
+        Se ejecuta al iniciar la sentencia WITH
+        :return:
+        """
+        print('Inicio del método with __enter__')
+        self._conn = Connection.get_connection()
+        self._cursor = self._conn.cursor()
+        return self._cursor
+
+    # exc_type = Tipo de excepción
+    # exc_val = Valor de la exepción (contenido)
+    # exc_tb = Exception traceback o traza de la excepción
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        Se ejecuta al finalizar la sentencia WITH
+        :param exc_type:
+        :param exc_val:
+        :param exc_tb:
+        :return:
+        """
+        print('Se ejecuta el método with __exit__')
+        if exc_val: # preguntamos si el valor de la excepción es diferente de None (no está vacía)
+            # si no está vacío significa que ocurrió una excepción y debemos hacer un rollback:
+            self._conn.rollback()
+            # a continuación enviamos a nuestro log los detalles de la excepción:
+            print(f'Ocurrió un error: {exc_type} {exc_val} {exc_tb}')
+        else:
+            # Si no ocurrió ninguna excepción  entonces mandamos a hacer el commit a la base de datos
+            self._conn.commit()
+            print('Commit de la transacción.')
+        self._cursor.close() # Finalizada la transacción cerramos el cursor
+
+
+if __name__ == '__main__':
+    with Cursor() as cursor:
+        print('Dentro del bloque with')
--- context/mysql_connection.py
+++ context/mysql_connection.py
@@ -1,5 +1,5 @@
-from mysql.connector import pooling
-class Conexion:
+from mysql import connector as con
+class Connection:
     """
     Clase encargada de la creación del pool de conexiones con la base de datos
     """
@@ -8,37 +8,28 @@ class Conexion:
     _PASSWORD = 'admin'
     _DB_PORT = '3306'
     _HOST = 'localhost'
-    _MAX_CON = 5 # define el máximo de objetos de conexión en memoria
-    _pool = None
+    _conn = None
 
     @classmethod
-    def obtenerPool(cls):
-        if cls._pool is None:
+    def get_connection(cls):
+        if cls._conn is None:
             try:
-                cls._pool = pooling.MySQLConnectionPool(
+                cls._conn = con.connect(
                     user=cls._USERNAME,
                     password=cls._PASSWORD,
                     host=cls._HOST,
                     port=cls._DB_PORT,
                     database=cls._DATABASE,
-                    pool_size=cls._MAX_CON
                 )
-                print(f'Creación del pool exitosa: {cls._pool}')
-                return cls._pool
+                print(f'Creación del pool exitosa: {cls._conn}')
+                return cls._conn
             except Exception as e:
                 print(f'Ocurrió un error: {e}')
         else:
-            return cls._pool
-
-    @classmethod
-    def obtenerConexion(cls):
-        # el método get_connection() devuelve un objeto de conexión del pool de conexiones.
-        conexion = cls.obtenerPool().get_connection()
-        print(f'Conexión obtenida del pool: {conexion}')
-        return conexion
+            return cls._conn
 
 
 if __name__ == '__main__':
     # Al ser métodos de clase podemos llamarlos sin necesidad de instanciarlos.
-    Conexion.obtenerConexion()
+    Connection.get_connection()
 
